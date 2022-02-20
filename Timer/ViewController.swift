a8a6ec174ac9fd6d3bf598786dc2d34f864d0326
Chernyshov Igor <chernyshov.ig@yandex.ru>
2022-02-20 12:10:09 +0300

    Life cycle -рыба

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //     MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
 
+    private func setupView() {
 
+    }
+
+    private func setupHierarchy() {
+
+    }
+
+    private func setupLayout() {
+
+    }
 }
 
